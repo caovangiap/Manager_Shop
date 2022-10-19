7cd61ca528e26a0ee7f08a64ffdb4d62501bd201
HLC_2021 <khongchovao@gmail.com>
2022-10-19 16:25:33 +0700

    commit order

--- app/src/main/java/com/example/android_basic_manager/view/addItems/FragmentAddItems.kt
+++ app/src/main/java/com/example/android_basic_manager/view/addItems/FragmentAddItems.kt
@@ -5,15 +5,25 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModel
+import com.example.android_basic_manager.MainActivity
+import com.example.android_basic_manager.databinding.FragmentAddItemsBinding
+import com.example.android_basic_manager.viewmodel.ViewModelAddItems
 
 class FragmentAddItems : Fragment() {
 
+    lateinit var binding : FragmentAddItemsBinding
+    lateinit var mView: View
+    lateinit var ViewModel : ViewModelAddItems
 
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return super.onCreateView(inflater, container, savedInstanceState)
+        binding = FragmentAddItemsBinding.inflate(inflater,container,false)
+        mView = binding.root
+        ViewModel = MainActivity.vMAddItems
+        return mView
     }
 }
---
+++ app/src/main/java/com/example/android_basic_manager/viewmodel/ViewModelAddItems.kt
@@ -0,0 +1,7 @@
+package com.example.android_basic_manager.viewmodel
+
+import androidx.lifecycle.ViewModel
+
+class ViewModelAddItems : ViewModel() {
+
+}
